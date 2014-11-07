da397f57dfa56b10caba31a17db884596a735df6
Klironce <gm-acherus@bk.ru>
2014-11-07 10:16:12 +0300

    first files

---
+++ btcetest/btcetest/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace btcetest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(216, 344);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(384, 70);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(384, 290);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(438, 48);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(384, 290);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(166, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(157, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(329, 12);
+            this.progressBar1.Maximum = 150;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(384, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 426);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ btcetest/btcetest/Parse.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace btcetest
+{
+    class Parse
+    {
+
+        public struct DataStek
+        {
+
+            public DataStek(string a)
+            {
+               skobcount = 0;
+               PMathcount = 0;
+               scob = new int[1,2];
+               pmath = new int[1];
+               foundSkobs(a);
+               createPMath(a);
+            }
+
+            private bool cheackAlfabet(char ch,bool first)
+            {
+                if (first)
+                {
+                    if ((ch == '*' || ch == ':'))
+                        return true;
+                    return false;
+                }
+                else
+                {
+                    if ((ch == '+' || ch == '-'))
+                        return true;
+                    return false;
+                }
+            }
+
+            public int skobcount;
+            public int[,] scob;
+            private void getPMathInSkob(int s, int f, string a)
+            {
+                for (int i = s + 1; i < f; i++)
+                    if (cheackAlfabet(a[i], true))
+                        addPMath(i);
+
+
+                for (int i = s + 1; i < f; i++)
+                    if (cheackAlfabet(a[i], false))
+                        addPMath(i);
+            }
+            private void addSkob(int p,string a)
+            {
+                int elseSkobCount = 0;
+                int pEnd = 0;
+                for (int i = p+1; i < a.Length; i++)
+                {
+                    if (a[i] == '(')
+                        elseSkobCount++;
+                    else
+                        if (a[i] == ')')
+                            if (elseSkobCount != 0)
+                                elseSkobCount--;
+                            else
+                            {
+                                pEnd = i;
+                                break;
+                            }
+
+                }
+
+                if(skobcount == 0)
+                {
+                    scob[0, 0] = p;
+                    scob[0, 1] = pEnd;
+                }
+                else
+                {
+                    int[,] backUp = new int[skobcount, 2];
+                    for (int i = 0; i < skobcount; i++)
+                    {
+                        backUp[i, 0] = scob[i, 0];
+                        backUp[i, 1] = scob[i, 1];
+                    }
+                    scob = new int[skobcount + 1, 2];
+                    for (int i = 0; i < skobcount; i++)
+                    {
+                        scob[i, 0] = backUp[i, 0];
+                        scob[i, 1] = backUp[i, 1]; 
+                    }
+                    scob[skobcount , 0] = p;
+                    scob[skobcount , 1] = pEnd;
+                }
+                skobcount++;
+            }
+            private void foundSkobs(string a)
+            {
+                for (int i = 0; i < a.Length; i++)
+                    if (a[i] == '(')
+                        addSkob(i, a);
+            }
+
+            public int PMathcount;
+            public int[] pmath;
+            private bool cheackPMathUsed(int p)
+            {
+                for (int i = 0; i < PMathcount; i++)
+                    if (pmath[i] == p)
+                        return false;
+
+                return true;
+            }
+            private void addPMath(int p)
+            {
+                if (cheackPMathUsed(p)) { 
+                    if (PMathcount == 0)
+                    {
+                        pmath[0] = p;
+                    }
+                    else
+                    {
+                        int[] backUp = new int[PMathcount];
+                        for (int i = 0; i < PMathcount; i++)
+                            backUp[i] = pmath[i];
+                        pmath = new int[PMathcount + 1];
+                        for (int i = 0; i < PMathcount; i++)
+                            pmath[i] = backUp[i];
+                        pmath[PMathcount] = p;
+                    }
+                    PMathcount++;
+                }
+            }
+            private void addOtherPMath(string a)
+            {
+                for (int i = 0; i < a.Length; i++)
+                    if (cheackAlfabet(a[i], true))
+                        addPMath(i);
+
+                for (int i = 0; i < a.Length; i++)
+                    if (cheackAlfabet(a[i], false))
+                        addPMath(i);
+            }
+            private void createPMath(string a)
+            {
+                if (skobcount != 0)
+                {
+                    for (int i = skobcount - 1; i != -1; i--)
+                    {
+                        getPMathInSkob(scob[i, 0], scob[i, 1], a);
+                    }
+
+                    addOtherPMath(a);
+                }
+            }
+
+        }
+
+        public static string getColumnName(string str, bool end)
+        {
+            int p = 0;
+            string nstr = "";
+            for (int i = 0; i < str.Length; i++)
+                if (str[i] == '_') p = i;
+
+                if (!end)
+                {
+                    for (int i = 0; i < p; i++)
+                        nstr += str[i];
+                }
+                else
+                {
+                    for (int i = p + 1; i < str.Length; i++)
+                        nstr += str[i];
+                }
+
+                return nstr;
+        }
+
+
+
+    }
+}
